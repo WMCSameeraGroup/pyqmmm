90c0a4a93cda82abc513b611827626e15f24abb5
madhurangar <mail.pvgm@gmail.com>
2021-04-06 18:59:50 +1000

    apply sed after coordinates section

--- fixes/fix_ice_g16x.sh
+++ fixes/fix_ice_g16x.sh
@@ -6,4 +6,11 @@
 
 # USAGE: sh fix_ice.sh FILENAME.EIn
 
-sed -i -E "s/\s+[0-9]+\s+0.100//g" $1
+# Make a copy of .EIn file to a backup folder
+#FNAME=`echo "$1" | awk -F'/' '{print $NF}'`
+#cp $1 /home/b/b32708/s1s12_amoeba/ein/${FNAME}
+
+# Remove partial connectivity from .EIn file (sed skips coordination section)
+EIN = $1
+NATOMS=`awk 'NR==1{print $1}' ${EIN}`
+sed -E "1,${NATOMS}b;s/\s+[0-9]+\s+0.100//g" ${EIN}
